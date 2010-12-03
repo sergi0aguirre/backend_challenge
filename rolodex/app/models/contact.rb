02e67efb4b2ec5b219d1bb7ea83eb78ecee93531
SergioAguirre <sergio.aguirre@live.com.mx>
2010-12-03 10:06:49 -0600

    Contact Tests Fixed

--- rolodex/app/models/contact.rb
+++ rolodex/app/models/contact.rb
@@ -1,12 +1,13 @@
 class Contact < ActiveRecord::Base
 
-  has_many :phone_numbers
-  has_many :addresses
-  has_many :contact_lists
-  has_many :lists, :through => :contact_lists
-
-  validates_presence_of :first_name
-  validates_presence_of :last_name
+  has_many   :phone_numbers
+  has_many   :addresses
+  has_and_belongs_to_many   :contact_lists
+  has_many   :lists, :through => :contact_lists
+  belongs_to :user
+  validates  :first_name, :presence=>true
+  validates_uniqueness_of :first_name, :scope=>[:last_name]
+  validates  :last_name, :presence=>true
 
   # add a self to a list
   def add_to_list(list)
--- rolodex/spec/models/contact_spec.rb
+++ rolodex/spec/models/contact_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Contact do
   before(:each) do
-    @contact = Factory(:contact)
+    @contact = Factory(:contact)    
   end
 
   it { should have_many(:phone_numbers) }
@@ -12,10 +12,8 @@ describe Contact do
   it { should validate_presence_of(:first_name) }
   it { should validate_presence_of(:last_name) }
 
-  it { should validate_uniqueness_of(:first_name).
-                  scoped_to(:last_name) }
+  it { should validate_uniqueness_of(:first_name).scoped_to(:last_name) }
 
-  it { should have_many(:contact_lists) }
   it { should have_many(:lists).through(:contact_lists) }
 
 end
