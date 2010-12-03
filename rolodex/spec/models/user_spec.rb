2a30a1f9572b379be6d7daf9884cd7e5b1fd8df2
SergioAguirre <sergio.aguirre@live.com.mx>
2010-12-03 11:14:57 -0600

    Add User specs

--- rolodex/spec/models/user_spec.rb
+++ rolodex/spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+   before(:each) do
+    @contact = Factory(:user)
+  end
+
+  it { should have_many(:lists) }
+  it { should have_many(:contacts) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
+  it { should validate_format_of(:email).with('5ddrema16@mail.com') }
+  it { should validate_format_of(:email).with('seam16_678.er@mail.com') }
+  it { should_not allow_value("blah").for(:email) }
+  it { should_not allow_value("blahdf@c.m").for(:email) }
+  it { should_not allow_value("1").for(:password) }
+  it { should allow_value("123456").for(:password) }
 end
