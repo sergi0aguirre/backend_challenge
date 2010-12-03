d0fbf63600fd8f27ac518ade5491404d87cceb5b
SergioAguirre <sergio.aguirre@live.com.mx>
2010-12-03 11:16:43 -0600

    first List specs

--- rolodex/app/models/list.rb
+++ rolodex/app/models/list.rb
@@ -1,4 +1,8 @@
 class List < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many   :contact_lists
+  has_many   :contacts, :through => :contact_lists
+
+  validates :name ,:presence=>true
+  
 end
--- rolodex/spec/models/list_spec.rb
+++ rolodex/spec/models/list_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe List do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @contact = Factory(:list)
+  end
+
+  it { should belong_to(:user) }
+  it { should validate_presence_of(:name) }
+  it { should have_many(:contacts).through(:contact_lists) }
 end
