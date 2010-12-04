e3bd0c988941f016cc2cf027aa52ae7af57575f9
SergioAguirre <sergio.aguirre@live.com.mx>
2010-12-04 17:36:47 -0600

    When you are in the contacts view, the page should be more wide

--- rolodex/app/controllers/contacts_controller.rb
+++ rolodex/app/controllers/contacts_controller.rb
@@ -1,6 +1,7 @@
 class ContactsController < ApplicationController
   before_filter :authenticate_user!
   def index
-    
+    @with_page='page_contact'
+    @contacts=current_user.contacts
   end
 end
