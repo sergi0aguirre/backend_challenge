d2080f5ea219664aca5922dcf779a4ff40f8ba86
SergioAguirre <sergio.aguirre@live.com.mx>
2010-12-10 05:15:44 -0600

    Contact Ajax CRUD

--- rolodex/app/controllers/contacts_controller.rb
+++ rolodex/app/controllers/contacts_controller.rb
@@ -1,7 +1,49 @@
 class ContactsController < ApplicationController
-  before_filter :authenticate_user!
-  def index
+  before_filter :authenticate_user!, :load
+
+  def load
     @with_page='page_contact'
-    @contacts=current_user.contacts
+    @contacts = current_user.contacts
+    @contact = Contact.new
+  end
+
+  def index
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.user=current_user
+    if @contact.save
+      flash[:notice] = "Successfully created contact."
+      @contacts = current_user.contacts
+    end
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update_attributes(params[:contact])
+      flash[:notice] = "Successfully updated contact."
+      @contacts = current_user.contacts
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    flash[:notice] = "Successfully destroyed contact."
+    @contacts = current_user.contacts
+  end
+
+  def search
+    @contacts=Contact.search("%"+params[:key]+"%",current_user.id)
   end
 end
+
---
+++ rolodex/public/javascripts/contacts.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+   $("#contact_list .contact_edit").click(function() {$('#contact_form').show();return false;});
+   $("#key").keyup(function() {$('#search').submit();});
+   $("#clear_search").click(function() {$('#key').val("");$('#search').submit();return false;});
+   
+});
