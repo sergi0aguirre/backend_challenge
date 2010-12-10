0c636fd48b5c1fc3b9f77a8161333af53108b0ab
SergioAguirre <sergio.aguirre@live.com.mx>
2010-12-10 04:45:18 -0600

    Change the relations between contacts and list

--- rolodex/app/models/contact.rb
+++ rolodex/app/models/contact.rb
@@ -2,12 +2,14 @@ class Contact < ActiveRecord::Base
 
   has_many   :phone_numbers
   has_many   :addresses
-  has_and_belongs_to_many   :contact_lists
-  has_many   :lists, :through => :contact_lists
+  has_many   :lists, :through => :contacts_lists
   belongs_to :user
   validates  :first_name, :presence=>true
   validates_uniqueness_of :first_name, :scope=>[:last_name]
   validates  :last_name, :presence=>true
+  validates  :user_id, :presence=>true
+
+  scope :search, lambda { |key|  where(["first_name LIKE ? OR middle_name LIKE ? OR last_name LIKE ?",key,key,key]) }
 
   # add a self to a list
   def add_to_list(list)
--- rolodex/app/models/list.rb
+++ rolodex/app/models/list.rb
@@ -1,7 +1,6 @@
 class List < ActiveRecord::Base
   belongs_to :user
-  has_and_belongs_to_many   :contact_lists
-  has_many   :contacts, :through => :contact_lists
+  has_many   :contacts, :through => :contacts_lists
 
   validates :name ,:presence=>true
   
