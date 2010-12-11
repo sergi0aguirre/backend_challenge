9ab3e5851ef0ad2a2f27a6f7a11a8f3f148ba298
SergioAguirre <sergio.aguirre@live.com.mx>
2010-12-11 13:56:57 -0600

    Set up Paperclip

--- rolodex/app/models/contact.rb
+++ rolodex/app/models/contact.rb
@@ -3,11 +3,17 @@ class Contact < ActiveRecord::Base
   has_many   :phone_numbers
   has_many   :addresses
   has_many   :lists, :through => :contacts_lists
+  has_many   :contacts_lists
   belongs_to :user
   validates  :first_name, :presence=>true
   validates_uniqueness_of :first_name, :scope=>[:last_name]
   validates  :last_name, :presence=>true
   validates  :user_id, :presence=>true
+  #Paperclip
+  has_attached_file :photo, :styles => {:thumb=> "20x20#",
+    :small  => "100x100>" },
+    :url  => "/assets/products/:id/:style/:basename.:extension",
+    :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
 
   scope :search, lambda { |key,user_id|  where(["(first_name LIKE ? OR middle_name LIKE ? OR last_name LIKE ?) AND user_id=?",key,key,key,user_id]) }
 
--- rolodex/config/environment.rb
+++ rolodex/config/environment.rb
@@ -1,5 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
+Paperclip::Railtie.insert
 # Initialize the rails application
 Rolodex::Application.initialize!
