729da13c13e0fc1a7bfd5c3b64601b3f31d7f601
SergioAguirre <sergio.aguirre@live.com.mx>
2010-12-11 23:56:57 -0600

    Push the Shema XD,  wow its time to fun the nigth start ,  i'll be back tommorrow , Ñ_Ñ

--- rolodex/db/schema.rb
+++ rolodex/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101013052956) do
+ActiveRecord::Schema.define(:version => 20101210131650) do
 
   create_table "address_types", :force => true do |t|
     t.string   "name"
@@ -30,10 +30,28 @@ ActiveRecord::Schema.define(:version => 20101013052956) do
     t.datetime "updated_at"
   end
 
+  create_table "contact_lists", :id => false, :force => true do |t|
+    t.integer  "contact_id"
+    t.integer  "list_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "contacts", :force => true do |t|
     t.string   "first_name"
     t.string   "middle_name"
     t.string   "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+  end
+
+  create_table "lists", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -52,5 +70,24 @@ ActiveRecord::Schema.define(:version => 20101013052956) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                       :default => "", :null => false
+    t.string   "reset_password_token"
+    t.string   "remember_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                       :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
