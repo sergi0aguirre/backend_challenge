da5808889caca089b9402336d8fd8a3b99505f34
SergioAguirre <sergio.aguirre@live.com.mx>
2010-12-14 06:14:09 -0600

    Modified the Load data for test

--- rolodex/spec/support/load_data_helper.rb
+++ rolodex/spec/support/load_data_helper.rb
@@ -1,33 +1,38 @@
 #
 # Creates a full data for test
 #
-# Don't forget to use fixtures :product_attributes on the spec
 #
 #
 def create_data_test
-  10.times do
+  5.times do
     user = User.make
     user.save
     #creeate and assing list
     5.times do
-      user.lists << List.make
+      list=List.make
+      list.save
+      user.lists << list
     end
     #create and assign contacts
-    15.times do
+    5.times do
     user.contacts << create_contact
     ContactList.create(:list => user.lists.first, :contact => user.contacts.last)
     ContactList.create(:list => user.lists.last, :contact => user.contacts.last)
     end
+    user.save
   end
 end
 
 def create_contact
   contact = Contact.make
   contact.save
-  contact.addresses << Address.make
-  contact.addresses << Address.make
-  contact.phone_numbers << PhoneNumber.make
-  contact.phone_numbers << PhoneNumber.make
+  address = Address.make
+  address.save
+  contact.addresses << address
+  phone = PhoneNumber.make
+  phone.save
+  contact.phone_numbers << phone
+  contact.save
   contact
 end
 
