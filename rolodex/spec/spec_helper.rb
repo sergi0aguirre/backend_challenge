aae94eb277c221fb955a09f0c9100c865572d239
SergioAguirre <sergio.aguirre@live.com.mx>
2010-12-14 01:36:35 -0600

    To load blueprints

--- rolodex/spec/spec_helper.rb
+++ rolodex/spec/spec_helper.rb
@@ -2,11 +2,12 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'webrat'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
+Dir[File.join(File.dirname(__FILE__), 'blueprints', '*_blueprint.rb')].each {|bp| require bp}
 RSpec.configure do |config|
   # == Mock Framework
   #
@@ -16,7 +17,7 @@ RSpec.configure do |config|
   # config.mock_with :flexmock
   # config.mock_with :rr
   config.mock_with :rspec
-
+  config.before(:each) { Machinist.reset_before_test }
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
